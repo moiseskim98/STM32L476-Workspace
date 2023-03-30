ae1da6dff0278d9aeff4a640f1818e53076bacd9
hkim-bitmec <hkim@bitmec.com.gt>
2023-03-30 11:02:47 -0600

    ignore

---
+++ 3_gpio_input/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ /home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ /home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ /home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/core_cm4.h \
+ /home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/cmsis_version.h \
+ /home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/cmsis_compiler.h \
+ /home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/cmsis_gcc.h \
+ /home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/mpu_armv7.h \
+ /home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+/home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+/home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+/home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/core_cm4.h:
+/home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/cmsis_version.h:
+/home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/cmsis_compiler.h:
+/home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/cmsis_gcc.h:
+/home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Include/mpu_armv7.h:
+/home/hkim/Documentos/STM32L476-Workspace/chip-header/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
