cb291ca08ce2e35bd9ba0f4c01d708812618f47b
hkim-bitmec <hkim@bitmec.com.gt>
2023-04-11 17:28:10 -0600

    modificadco hasta la fecha. aun no funciona adc

---
+++ 8_adc/Src/adc.c
@@ -0,0 +1,85 @@
+#include "adc.h"
+#include "stm32l4xx.h"
+
+#define AHB2ENR_ADCEN			(1U<<13)
+#define AHB2ENR_GPIOCEN			(1U<<2)
+
+#define GPIOx_MODER0_0			(1U<<0)
+#define GPIOx_MODER0_1			(1U<<1)
+
+#define SQR1_L_0				(1U<<0)
+#define SQR1_L_1				(1U<<1)
+#define SQR1_L_2				(1U<<2)
+#define SQR1_L_3				(1U<<3)
+#define SQR1_SQ1_0				(1U<<6)
+#define SQR1_SQ1_1				(1U<<7)
+#define SQR1_SQ1_2				(1U<<8)
+#define SQR1_SQ1_3				(1U<<9)
+#define SQR1_SQ1_4				(1U<<10)
+
+#define IER_ADRDYIE				(1U<<0)
+
+#define ISR_ADRDY				(1U<<0)
+#define ISR_EOC					(1U<<2)
+
+#define CR_ADEN					(1U<<0)
+#define CR_ADSTART				(1U<<2)
+#define CR_ADVREGEN				(1U<<28)
+#define CR_DEEPPWD				(1U<<29)
+
+
+
+// CANAL 1
+void pc0_adc1_init(void)
+{
+	// Configuracion del clk para GPIOC
+	RCC->AHB2ENR |= AHB2ENR_GPIOCEN;
+
+	// Configurar PC0 como analogo
+	GPIOC->MODER |= (GPIOx_MODER0_1|GPIOx_MODER0_0);
+
+
+	//******Configuracion del modulo de ADC******
+
+	// Configuracion del reloj para el modulo
+	RCC->AHB2ENR |= AHB2ENR_ADCEN;
+
+	// Salir del modo deep-power-down
+	ADC1->CR &= ~(CR_DEEPPWD);
+
+	// Habilitar el regulador del adc
+	ADC1->CR |= CR_ADVREGEN;
+	//for(int i = 0; i < 100000; i++){}
+
+	// Configuracion del inicio de la secuencia de conversion
+	ADC1->SQR1 &= ~(SQR1_SQ1_1|SQR1_SQ1_2|SQR1_SQ1_3|SQR1_SQ1_4);
+	ADC1->SQR1 |= SQR1_SQ1_0;
+
+	// Configuración de la longitud de la secuencia de conversion
+	ADC1->SQR1 &= ~(SQR1_L_0|SQR1_L_1|SQR1_L_2|SQR1_L_3);
+
+	// Limpiar la bandera de conversión del adc
+	ADC1->ISR |= ISR_ADRDY;
+
+	// Inicializa el adc
+	ADC1->CR |= CR_ADEN;
+
+	// Esperar que inicializó
+	while((ADC1->ISR & ISR_ADRDY) == 0){ADC1->ISR |= ISR_ADRDY;}
+
+}
+
+void start_adc1_conversion(void)
+{
+	// Comienza la conversión
+	ADC1->CR |= CR_ADSTART;
+}
+
+uint32_t adc1_read(void)
+{
+	// Esperar a que termine la conversión
+	while(!(ADC1->ISR & ISR_EOC)){}
+
+	// Lee la conversion
+	return (ADC1->DR);
+}
