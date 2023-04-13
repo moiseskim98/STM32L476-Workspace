1a4eb76d694efa3e3c737234c013f54e0594f58a
hkim-bitmec <hkim@bitmec.com.gt>
2023-04-13 10:04:49 -0600

    ya funciona el timer 2

---
+++ 11_Timers/Inc/tim.h
@@ -0,0 +1,15 @@
+/*
+ * tim.h
+ *
+ *  Created on: 12/04/2023
+ *      Author: hkim
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+
+#define SR_UIF		(1U<<0)
+
+void tim2_1hz_init(void);
+
+#endif /* TIM_H_ */
---
+++ 11_Timers/Src/tim.c
@@ -0,0 +1,26 @@
+#include "stm32l4xx.h"
+
+
+#define APB1ENR_TIM2EN			(1U<<0)
+
+#define	CR1_CEN					(1U<<0)
+
+
+void tim2_1hz_init(void)
+{
+	// Habilitar el acceso al reloj al timer 2
+	RCC->APB1ENR1 |= APB1ENR_TIM2EN;
+
+	// Setear el prescaler
+	TIM2->PSC = 400 - 1;	// 4 000 000 / 400 = 10 000
+
+	// Setear el valor de auto-recarga
+	TIM2->ARR = 10000 - 1;	//	10 000/10 000 = 1
+
+	// Limpiar el contador
+	TIM2->CNT = 0;
+
+	// Habilitar el timer
+	TIM2->CR1 |= CR1_CEN;
+
+}
