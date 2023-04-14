6891d0d32c392b636bdeb60b3fa23f86f13f5db2
hkim-bitmec <hkim@bitmec.com.gt>
2023-04-14 13:00:04 -0600

    jalo el de SysTick

---
+++ 17_Systick_Interrupt/Src/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32l4xx.h"
+#include "uart.h"
+#include "systick.h"
+
+#define PIN5				(1U<<5)
+#define PIN13				(1U<<13)
+
+#define LED_PIN				PIN5	// PA5
+#define PUSH_BUTTON			PIN13	// PC13
+
+#define AHB2ENR_GPIOAEN		(1U<<0)
+#define AHB2ENR_GPIOCEN		(1U<<2)
+
+#define MODER_MODE5_0		(1U<<10)
+#define MODER_MODE5_1		(1U<<11)
+#define MODER_MODE13_0		(1U<<26)
+#define MODER_MODE13_1		(1U<<27)
+
+uint32_t hola;
+char received;
+
+int main(void)
+{
+	//-------PA5 configuration----------
+	RCC->AHB2ENR |= AHB2ENR_GPIOAEN;
+	GPIOA->MODER |= MODER_MODE5_0;
+	GPIOA->MODER &= ~MODER_MODE5_1;
+	//----------------------------------
+
+	//--------PC13 configuration--------
+	RCC->AHB2ENR |= AHB2ENR_GPIOCEN;
+	GPIOC->MODER &= ~(MODER_MODE13_0|MODER_MODE13_1);
+	//----------------------------------
+
+	//--------USART2--------------------
+	uart2_rxtx_init();
+	systick_1hz_interrupt();
+
+
+	while(1)
+	{
+		systickDelayMs(1000);
+		GPIOA->ODR ^= LED_PIN;
+		printf("paso un segundo!!!!\n\r");
+
+		/*
+		received = uart2_read();
+		if(received == '1')
+		{
+			GPIOA->ODR |=  LED_PIN;
+		}
+		else
+		{
+			GPIOA->ODR &= ~LED_PIN;
+		}
+		*/
+		/*
+		hola = GPIOC->IDR;
+		if(hola & PUSH_BUTTON)
+		{
+			GPIOA->ODR |= LED_PIN;
+		}
+		else
+		{
+			GPIOA->ODR &= ~LED_PIN;
+			printf("hello from the other side!!!!\n\r");
+		}
+		*/
+
+	}
+
+	return 1;
+}
+
+static void systick_callback(void)
+{
+	GPIOA->ODR ^= LED_PIN;
+	printf("paso un segundo!!!!\n\r");
+}
+
+void SysTick_Handler(void)
+{
+	// Hace algo
+	systick_callback();
+}
+
+
